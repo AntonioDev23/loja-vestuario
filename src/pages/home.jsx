6f322acd1af10414f23d6f4f54e8a7b3505a444c
ANTONIO  NETO <snetinho40@gmail.com>
2025-08-27 18:05:16 -0300

    up

---
+++ src/pages/home.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import '../styles/Home.css';
+
+function Home() {
+  return (
+    <section className="home-presentation">
+      <div className="presentation-content">
+        <h1>Bem-vindo ao meu site!</h1>
+        <p>Apresentação breve sobre o site e seu propósito.</p>
+      </div>
+    </section>
+  );
+}
+
+export default Home;
