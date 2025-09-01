aa5018a1b52792e6fd15684df97857428982fe22
ANTONIO  NETO <snetinho40@gmail.com>
2025-09-01 18:28:05 -0300

    up

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -1,7 +1,8 @@
+// src/pages/home.jsx
 import React from 'react';
 import '../styles/Home.css';
-import Navbar from './components/Navbar';
-import Hero from './components/Hero';
+import Navbar from '../components/Navbar';
+import Hero from '../components/Hero';
 import ProductCard from '../components/ProductCard';
 import Newsletter from '../components/Newsletter';
 import Footer from '../components/Footer';
@@ -14,19 +15,27 @@ import prod3 from '../assets/prod3.png';
 function Home() {
   return (
     <div className="home-page">
+      {/* Cabeçalho */}
       <Navbar />
+
+      {/* Seção de apresentação */}
       <Hero />
 
+      {/* Seção de produtos */}
       <section className="products-section">
         <ProductCard image={prod1} name="Camisa Casual" price="79,90" />
         <ProductCard image={prod2} name="Calça Jeans" price="129,90" />
         <ProductCard image={prod3} name="Tênis Esportivo" price="199,90" />
       </section>
 
+      {/* Newsletter */}
       <Newsletter />
+
+      {/* Rodapé */}
       <Footer />
     </div>
   );
 }
 
 export default Home;
+
