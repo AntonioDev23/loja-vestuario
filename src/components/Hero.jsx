11e713436f9220ba57241b41bee19cdeb3211a87
ANTONIO  NETO <snetinho40@gmail.com>
2025-09-01 17:55:06 -0300

    up

---
+++ src/components/Hero.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import '../styles/Hero.css';
+
+function Hero() {
+  return (
+    <section className="hero">
+      <h1>Bem-vindo à Nossa Loja</h1>
+      <p>Confira nossas últimas coleções de moda!</p>
+      <button>Ver Produtos</button>
+    </section>
+  );
+}
+
+export default Hero;
