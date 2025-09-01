b0fba4864c07ddde330be3d6367846e97c11a182
ANTONIO  NETO <snetinho40@gmail.com>
2025-09-01 17:56:03 -0300

    up

---
+++ src/components/Footer.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import '../styles/Footer.css';
+
+function Footer() {
+  return (
+    <footer className="footer">
+      <p>© 2025 Antônio Ricardo da Silva Neto</p>
+      <ul>
+        <li>Sobre</li>
+        <li>Contato</li>
+        <li>Instagram</li>
+        <li>GitHub</li>
+        <li>LinkedIn</li>
+      </ul>
+    </footer>
+  );
+}
+
+export default Footer;
