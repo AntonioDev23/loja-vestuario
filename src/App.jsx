1a4ca6e34c7b11b25f78b15ffeab17cd573c5d07
ANTONIO  NETO <snetinho40@gmail.com>
2025-10-13 19:09:25 -0300

    up

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import './styles/index.css'; // mantém o CSS global
 import Home from './pages/Home';
 import Login from './pages/Login';
+import Cart from './pages/Cart'; // importando a página do carrinho
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/login" element={<Login />} />
+          <Route path="/cart" element={<Cart />} /> {/* rota do carrinho */}
           {/* Futuras páginas podem ser adicionadas aqui */}
         </Routes>
       </div>
