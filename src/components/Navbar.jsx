ec4b7481c3623af5ffae857cd33cd51c9ff0e68e
ANTONIO  NETO <snetinho40@gmail.com>
2025-10-15 19:42:03 -0300

    Update Navbar.jsx

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,31 +2,40 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import '../styles/Navbar.css';
 import logo from '../assets/logo.png';
-import { User, ShoppingCart, Search } from 'lucide-react';
+
 
 function Navbar() {
   return (
     <nav className="navbar">
-      {/* Logo à esquerda */}
-      <div className="logo">
-        <img src={logo} alt="Logo da Loja" />
+      {/* Linha superior: logo, menu e ícones */}
+      <div className="navbar-top">
+        {/* Logo */}
+        <div className="logo">
+          <img src={logo} alt="Logo da Loja" />
+        </div>
+
+        {/* Menu de navegação */}
+        <ul className="menu">
+          <li><Link to="/">Home</Link></li>
+          <li><Link to="/produtos">Produtos</Link></li>
+          <li><Link to="/colecoes">Coleções</Link></li>
+        </ul>
+
+        {/* Ícones: Login e Carrinho */}
+        <div className="navbar-icons">
+          <Link to="/login">
+            <button>Login</button>
+          </Link>
+          <Link to="/carrinho">
+            <button>Carrinho</button>
+          </Link>
+        </div>
       </div>
 
-      {/* Barra de busca central */}
-      <div className="search-bar">
-        <input type="text" placeholder="Buscar..." />
-        <Search size={20} className="icon search-icon" />
-      </div>
-
-      {/* Ícones à direita */}
-      <div className="navbar-icons">
-        <Link to="/login">
-          <User size={24} className="icon" />
-        </Link>
-
-        <Link to="/carrinho">
-          <ShoppingCart size={24} className="icon" />
-        </Link>
+      {/* Barra de busca centralizada abaixo do menu */}
+      <div className="navbar-search">
+        <input className="search-input" type="text" placeholder="Buscar..." />
+        <button className="search-btn">Buscar</button>
       </div>
     </nav>
   );
