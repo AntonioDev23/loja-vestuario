d623ce91bb291f854c064a66d1b1ff0e4ac826ad
ANTONIO  NETO <snetinho40@gmail.com>
2025-10-15 19:39:22 -0300

    up

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -12,24 +12,18 @@ function Navbar() {
         <img src={logo} alt="Logo da Loja" />
       </div>
 
-      {/* Menu central */}
-      <ul className="menu">
-        <li>Home</li>
-        <li>Produtos</li>
-        <li>Coleções</li>
-      </ul>
+      {/* Barra de busca central */}
+      <div className="search-bar">
+        <input type="text" placeholder="Buscar..." />
+        <Search size={20} className="icon search-icon" />
+      </div>
 
       {/* Ícones à direita */}
       <div className="navbar-icons">
-        {/* Ícone de busca */}
-        <Search size={24} className="icon" />
-
-        {/* Link para login */}
         <Link to="/login">
           <User size={24} className="icon" />
         </Link>
 
-        {/* Link para carrinho */}
         <Link to="/carrinho">
           <ShoppingCart size={24} className="icon" />
         </Link>
