11ad67a74bb841a73299de2099aebc969a6171e0
ANTONIO  NETO <snetinho40@gmail.com>
2025-11-24 20:40:57 -0300

    up

--- src/data/colecoes.js
+++ src/data/colecoes.js
@@ -2,65 +2,62 @@
 import { produtosMasculinos } from '../pages/produtosMasculinos';
 import { produtosFemininos } from '../pages/produtosFemininos';
 
-import masculina from "../assets/imagens3/masculina.jpg";
-import feminina from "../assets/imagens3/feminina.jpg";
-import esportiva from "../assets/imagens3/esportiva.jpg";
-import inverno from "../assets/imagens3/inverno.jpg";
+import masculina from "../assets/imagens3/masculina.png";
+import feminina from "../assets/imagens3/feminina.png";
+import esportiva from "../assets/imagens3/esportiva.png";
+import inverno from "../assets/imagens3/inverno.png";
 
 export const colecoes = [
   {
-    id: 'streetwear',
-    nome: 'Streetwear',
-    banner: masculina,  
-    descricao: 'Estilo urbano, ousado e atual.',
+    id: 'masculina',
+    nome: 'Coleção Masculina',
+    banner: masculina,
+    descricao: 'Estilo moderno, urbano e elegante para eles.',
     produtos: [
       produtosMasculinos[0],
       produtosMasculinos[1],
       produtosMasculinos[2],
+      produtosMasculinos[3],
+      produtosMasculinos[4],
+    ]
+  },
+  {
+    id: 'feminina',
+    nome: 'Coleção Feminina',
+    banner: feminina,
+    descricao: 'Moda atual, leve e estilosa para elas.',
+    produtos: [
       produtosFemininos[0],
       produtosFemininos[1],
       produtosFemininos[2],
+      produtosFemininos[3],
+      produtosFemininos[4],
     ]
   },
   {
-    id: 'inverno-2025',
-    nome: 'Inverno 2025',
-    banner: inverno,  
-    descricao: 'Casacos, botas e looks quentes para o frio.',
+    id: 'esportiva',
+    nome: 'Esporte & Performance',
+    banner: esportiva,
+    descricao: 'Roupas leves, resistentes e de alta performance.',
     produtos: [
-      produtosMasculinos[5],
-      produtosMasculinos[6],
-      produtosMasculinos[7],
+      produtosMasculinos[10],
+      produtosMasculinos[11],
+      produtosMasculinos[12],
       produtosFemininos[10],
       produtosFemininos[11],
-      produtosFemininos[12],
     ]
   },
   {
-    id: 'verao-2025',
-    nome: 'Verão 2025',
-    banner: feminina,  
-    descricao: 'Cores fortes e roupas leves para o calor.',
+    id: 'inverno',
+    nome: 'Coleção Inverno',
+    banner: inverno,
+    descricao: 'Casacos e peças quentes para os dias frios.',
     produtos: [
-      produtosMasculinos[15],
-      produtosMasculinos[16],
-      produtosFemininos[15],
-      produtosFemininos[16],
-      produtosFemininos[17],
-    ]
-  },
-  {
-    id: 'esporte-performance',
-    nome: 'Esporte & Performance',
-    banner: esportiva,  
-    descricao: 'Roupas leves e resistentes para alta performance.',
-    produtos: [
-      produtosMasculinos[20],
-      produtosMasculinos[21],
-      produtosMasculinos[22],
-      produtosFemininos[20],
-      produtosFemininos[21],
-      produtosFemininos[22],
+      produtosMasculinos[6],
+      produtosMasculinos[7],
+      produtosMasculinos[8],
+      produtosFemininos[6],
+      produtosFemininos[7],
     ]
   }
 ];
