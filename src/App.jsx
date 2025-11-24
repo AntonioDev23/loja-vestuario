345100904f086b66fec554a9c6a58e198178b4d8
ANTONIO  NETO <snetinho40@gmail.com>
2025-11-24 18:49:19 -0300

    up

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,14 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import './styles/index.css'; // mantém o CSS global
+import './styles/index.css'; 
 import Home from './pages/Home';
 import Login from './pages/Login';
-import Carrinho from './pages/carrinho'; // importando a página do carrinho
+import Carrinho from './pages/carrinho';
 import Produtos from './pages/Produtos';
+
+// 🆕 Importando as novas páginas
+import Colecoes from './pages/colecoes';
+import ColecoesDetalhes from './pages/colecoesdetalhes';
 
 function App() {
   return (
@@ -13,8 +17,12 @@ function App() {
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/login" element={<Login />} />
-          <Route path="/carrinho" element={<Carrinho />} /> {/* rota do carrinho */}
-          <Route path="/produtos" element={<Produtos />} /> {/* rota do produtos */}
+          <Route path="/carrinho" element={<Carrinho />} />
+          <Route path="/produtos" element={<Produtos />} />
+
+          {/* 🆕 Rotas das coleções */}
+          <Route path="/colecoes" element={<Colecoes />} />
+          <Route path="/colecoes/:id" element={<ColecoesDetalhes />} />
         </Routes>
       </div>
     </Router>
