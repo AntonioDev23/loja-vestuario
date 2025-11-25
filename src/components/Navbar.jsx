d4f5200b9a18f18846e5799d70efda01627ca449
ANTONIO  NETO <snetinho40@gmail.com>
2025-11-25 20:21:50 -0300

    up

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,37 +1,56 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import { Link } from 'react-router-dom';
 import '../styles/Navbar.css';
 import logo from '../assets/logo.png';
 import { User, ShoppingCart, Search } from 'lucide-react';
+import { CartContext } from '../context/cartcontext'; // <-- IMPORTANTE
 
 function Navbar() {
+  const { cart } = useContext(CartContext);
+
+  // Soma total de itens
+  const totalItens = cart?.reduce((acc, item) => acc + item.quantity, 0) || 0;
+
   return (
     <nav className="navbar">
       <div className="navbar-top">
+
         {/* Logo à esquerda */}
         <div className="logo">
           <img src={logo} alt="Logo da Loja" />
         </div>
 
         {/* Menu central */}
-      <ul className="menu">
-        <li><Link to="/">Home</Link></li>
-        <li><Link to="/produtos">Produtos</Link></li>
-        <li><Link to="/colecoes">Coleções</Link></li>
-      </ul>
+        <ul className="menu">
+          <li><Link to="/">Home</Link></li>
+          <li><Link to="/produtos">Produtos</Link></li>
+          <li><Link to="/colecoes">Coleções</Link></li>
+        </ul>
 
         {/* Ícones à direita */}
         <div className="navbar-icons">
+
+          {/* Login */}
           <Link to="/login">
             <User size={24} className="icon" />
           </Link>
-          <Link to="/carrinho">
-            <ShoppingCart size={24} className="icon" />
-          </Link>
+
+          {/* Carrinho com badge */}
+          <div className="cart-icon-wrapper">
+            <Link to="/carrinho">
+              <ShoppingCart size={24} className="icon" />
+            </Link>
+
+            {totalItens > 0 && (
+              <span className="cart-badge">
+                {totalItens}
+              </span>
+            )}
+          </div>
         </div>
       </div>
 
-      {/* Barra de busca abaixo do menu */}
+      {/* Barra de busca abaixo */}
       <div className="navbar-search">
         <input type="text" className="search-input" placeholder="Buscar..." />
         <button className="search-btn">
